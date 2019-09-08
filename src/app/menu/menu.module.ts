c38ffe0459a8de2ab00cdb9fbb4a616beba42015
Desktop <manualsplash@gmail.com>
2019-09-08 22:36:11 +0530

    Add routes to menu

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -6,11 +6,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { MenuPage } from './menu.page';
+import { MenuTitleComponentModule } from '../components/menu-title/menu-title.module';
+import { HomePage } from '../home/home.page';
+import { ListPage } from '../list/list.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'list',
+    component: ListPage
   }
 ];
 
@@ -19,8 +30,9 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MenuTitleComponentModule
   ],
-  declarations: [MenuPage]
+  declarations: [MenuPage, HomePage, ListPage]
 })
 export class MenuPageModule {}
