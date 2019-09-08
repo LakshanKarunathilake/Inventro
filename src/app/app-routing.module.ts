a7089313e2b742cb4f89de4a08dd0b75eb98e732
Desktop <manualsplash@gmail.com>
2019-09-08 22:11:42 +0530

    Format routing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' }
+
 ];
 
 @NgModule({
