a633ab723e479057363da684b4f93f4b9e1bf0b8
Desktop <manualsplash@gmail.com>
2019-09-09 23:08:12 +0530

    Add login functionality and error detection

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,89 @@
+import { LoadingController, ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
+  loginForm: FormGroup;
+  loading;
+  /**
+   *
+   * @param fb Form builder for building the forms in angular material
+   * @param router Routing service for the ionic navigation
+   * @param loadingController Loading component service to preview preloader
+   * @param modalController Modal service to present the modal
+   */
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private loadingController: LoadingController,
+    private modalController: ModalController
+  ) {
+    this.loadingController.create({
+      message: 'Please wait verifying!',
+      duration: 2000
+    });
+  }
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      userEmail: ['', [Validators.required, Validators.email]],
+      userPassword: ['', Validators.required]
+    });
   }
 
+  public hasError(controlName: string, errorName: string) {
+    return this.loginForm.controls[controlName].hasError(errorName);
+  }
+
+  loginAction = async () => {
+    // const userEmail = this.loginForm.controls['userEmail'].value;
+    // const userPassword = this.loginForm.controls['userPassword'].value;
+    // const loading = await this.loadingController.create({
+    //   message: 'Veryfying your details!'
+    // });
+
+    this.router.navigateByUrl('menu', { replaceUrl: true });
+  };
+
+  getErrorMessage = (controller: string) => {
+    const formController = this.loginForm.controls[controller];
+    return formController.hasError('required')
+      ? 'You must enter a value'
+      : formController.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  };
+
+  checkFormControlIsValid(control: string) {
+    return this.loginForm.controls[control].invalid;
+  }
+
+  previewErrorMessage = ({ code, message }) => {
+    const swalProps = { title: '', description: '' };
+    switch (code) {
+      case 'network-request-failed':
+        swalProps.description = 'Check your internet Connection';
+        swalProps.title = 'Connection Failure';
+        break;
+      case 'auth/user-disabled':
+        swalProps.description =
+          'Your connection is disabled please contact authority';
+        swalProps.title = 'Account disabled';
+        break;
+      case 'auth/wrong-password':
+        swalProps.description = 'Your password is incorrect';
+        swalProps.title = 'Wrong Password';
+        break;
+      default:
+        console.log('executing default');
+        swalProps.description = message;
+        swalProps.title = 'Error';
+        break;
+    }
+  };
 }
