500e254da4df4a76003184f803e2680c9f60c9b4
Desktop <manualsplash@gmail.com>
2019-09-11 23:12:20 +0530

    Create new employee page

---
+++ src/app/new-employee/new-employee.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NewEmployeePage } from './new-employee.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NewEmployeePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [NewEmployeePage]
+})
+export class NewEmployeePageModule {}
---
+++ src/app/new-employee/new-employee.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-employee',
+  templateUrl: './new-employee.page.html',
+  styleUrls: ['./new-employee.page.scss'],
+})
+export class NewEmployeePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
