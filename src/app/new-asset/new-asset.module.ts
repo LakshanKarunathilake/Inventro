5653a1f830e66adc00fa59082a0b04d1af0c14a3
Desktop <manualsplash@gmail.com>
2019-09-11 23:11:45 +0530

    Create new asset page

---
+++ src/app/new-asset/new-asset.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NewAssetPage } from './new-asset.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NewAssetPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [NewAssetPage]
+})
+export class NewAssetPageModule {}
---
+++ src/app/new-asset/new-asset.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-asset',
+  templateUrl: './new-asset.page.html',
+  styleUrls: ['./new-asset.page.scss'],
+})
+export class NewAssetPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
