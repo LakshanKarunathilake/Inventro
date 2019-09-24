712ea7bcce1936d783deb58b5bd49a9e26b2536d
Lakshan Karunathilake <manualsplash@gmail.com>
2019-09-24 18:32:33 +0530

    Implement function to create new employee

--- src/app/services/Employee/employee.service.ts
+++ src/app/services/Employee/employee.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Employee } from 'src/models/Employee';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,20 @@ import { Injectable } from '@angular/core';
 export class EmployeeService {
 
   constructor() { }
+
+
+  createNewEmployee = (employee: Employee) => {
+    const url = 'http://localhost:8080/employees/add';
+    fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(employee) // body data type must match "Content-Type" header
+    })
+      .then(response => response.json())
+      .then(data => console.log('data', data))
+      .catch(error => console.log('error', error)); // parses JSON response into native JavaScript objects
+  };
+  }
 }
