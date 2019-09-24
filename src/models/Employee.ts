9c78e6962482d4c4f57c2c22aa26ec58523e09cc
Lakshan Karunathilake <manualsplash@gmail.com>
2019-09-24 17:48:18 +0530

    Create employee model

---
+++ src/models/Employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  employeeId?: string; // not necessary
+  firstname: string;
+  lastname: string;
+  nic: string;
+  gender: string;
+  email: string;
+  password: string;
+  contactNo: number;
+  address: string;
+  status: string;
+}
