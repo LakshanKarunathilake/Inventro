3f5a778e90ef34177c6ce98792bfa532ac97983b
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-05 11:04:19 +0530

    Create form control group

--- src/app/new-asset/new-asset.page.ts
+++ src/app/new-asset/new-asset.page.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-asset.page.scss'],
 })
 export class NewAssetPage implements OnInit {
+  newAsset: FormGroup;
+  categories = [
+    'OTHER',
+    'LAPTOP',
+    'PC',
+    'PROJECTOR',
+    'UPS',
+    'FURNITURE',
+    'WATER FILTER',
+    'MIKE',
+    'SOUND SYSTEM',
+    'DISPLAY'
+  ];
+  constructor(private fb: FormBuilder) {
+    this.newAsset = this.fb.group({
+      assetCategory: ['', Validators.required],
+      brandName: ['', Validators.required],
+      boughtCompanyName: ['', Validators.required],
+      buyingPrice: ['', [Validators.required]],
+      warrantyStatus: ['', Validators.required],
+      boughtDate: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
 
   constructor() { }
 
