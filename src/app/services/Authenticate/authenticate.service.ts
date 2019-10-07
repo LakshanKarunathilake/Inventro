b2fef15fb95f4f253e9737a9bbaa2deaf58b8784
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-07 23:55:09 +0530

    Update http call using AES encryption

--- src/app/services/Authenticate/authenticate.service.ts
+++ src/app/services/Authenticate/authenticate.service.ts
@@ -1,24 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as crypto from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   loginUser = (username, password) => {
-    // Example POST method implementation:
-    const url = 'http://localhost:8080/employees/login';
-    const data = { username, password };
-    fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    })
-      .then(response => response.json())
-      .then(data => console.log('data', data))
-      .catch(error => console.log('error', error)); // parses JSON response into native JavaScript objects
+    const url = 'http://localhost:8080/authenticate';
+    const encryptedpsd = crypto.AES.encrypt(
+      password,
+      '1234567890123456'
+    ).toString();
+    console.log('encryptedpsd', encryptedpsd);
+    this.http
+      .post(url, { username, encryptedpsd })
+      .subscribe(data => console.log('data', data));
   };
 }
