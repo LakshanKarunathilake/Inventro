31d60eeb8c17d068b34ec8d83537ae26ec15b4fa
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-07 23:55:24 +0530

    Implement interceptor

--- src/app/services/AuthInterceptor/auth-interceptor.service.ts
+++ src/app/services/AuthInterceptor/auth-interceptor.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
 
-  constructor() { }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log(req.url);
+
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      const clonedReq = req.clone({
+        // headers:req.headers.set("Authorization","Bearer "+token)
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log('auth header added');
+      return next.handle(clonedReq);
+    } else {
+      console.log('auth header not added');
+      return next.handle(req);
+    }
+  }
 }
