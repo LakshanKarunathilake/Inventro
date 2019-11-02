55b4d3071e38b7fc10f2539346fb876b308bae23
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-02 13:32:53 +0530

    Send plain password to backend

--- src/app/services/Authenticate/authenticate.service.ts
+++ src/app/services/Authenticate/authenticate.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as crypto from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +9,8 @@ export class AuthenticateService {
 
   loginUser = (username, password) => {
     const url = 'http://localhost:8080/authenticate';
-    const encryptedpsd = crypto.AES.encrypt(
-      password,
-      '1234567890123456'
-    ).toString();
-    console.log('encryptedpsd', encryptedpsd);
     this.http
-      .post(url, { username, encryptedpsd })
+      .post(url, { username, password })
       .subscribe(data => console.log('data', data));
   };
 }
