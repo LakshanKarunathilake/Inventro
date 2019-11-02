aa89b13dab5fd4c6fbc355ed02e72e089fca840e
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-02 15:50:04 +0530

    Add auth guards

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { MenuPage } from './menu.page';
 import { MenuTitleComponentModule } from '../components/menu-title/menu-title.module';
+import { AuthGuardGuard } from '../guards/auth-guard.guard';
 const routes: Routes = [
   {
     path: '',
@@ -15,22 +16,26 @@ const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       {
         path: 'home',
-        loadChildren: '../home/home.module#HomePageModule'
+        loadChildren: '../home/home.module#HomePageModule',
+        canActivate: [AuthGuardGuard]
       },
       { path: 'pool', loadChildren: '../pool/pool.module#PoolPageModule' },
       {
         path: 'employee',
         loadChildren:
-          '../new-employee/new-employee.module#NewEmployeePageModule'
+          '../new-employee/new-employee.module#NewEmployeePageModule',
+        canActivate: [AuthGuardGuard]
       },
       {
         path: 'asset',
-        loadChildren: '../new-asset/new-asset.module#NewAssetPageModule'
+        loadChildren: '../new-asset/new-asset.module#NewAssetPageModule',
+        canActivate: [AuthGuardGuard]
       },
       {
         path: 'notifications',
         loadChildren:
-          '../notifications/notifications.module#NotificationsPageModule'
+          '../notifications/notifications.module#NotificationsPageModule',
+        canActivate: [AuthGuardGuard]
       }
     ]
   }
