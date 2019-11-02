d0871bcce031c6b301db7dc7731d69829792c548
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-02 16:24:15 +0530

    Update with swal messages

--- src/app/services/Employee/employee.service.ts
+++ src/app/services/Employee/employee.service.ts
@@ -2,19 +2,30 @@ import { Injectable } from '@angular/core';
 import { Employee } from 'src/models/Employee';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { SwalService } from '../swal/swal.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private swal: SwalService) {}
 
   createNewEmployee = (employee: Employee) => {
     const url = `${environment.backendURL}/employees/add`;
     this.http
       .post<Employee>(url, employee)
       .toPromise()
-      .then(data => console.log('data', data))
-      .catch(error => console.log('error', error)); // parses JSON response into native JavaScript objects
+      .then(() => {
+        this.swal.viewSuccessMessage(
+          'Success',
+          'Employee added successfully !'
+        );
+      })
+      .catch(error =>
+        this.swal.viewErrorMessage(
+          'Error',
+          'Sorry we could not place your request'
+        )
+      ); // parses JSON response into native JavaScript objects
   };
 }
