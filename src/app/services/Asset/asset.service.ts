da2c22ebb51df69576ce5cb821c5a029aefce9e8
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-05 10:00:18 +0530

    Get all assets

--- src/app/services/Asset/asset.service.ts
+++ src/app/services/Asset/asset.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Asset } from 'src/models/Asset';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssetService {
-  hostURL = 'http://localhost:8080/';
   constructor(private http: HttpClient) {}
 
   addNewAsset = (asset: Asset) => {
-    const subURl = 'asset/add';
+    const url = `${environment.backendURL}asset/add`;
     this.http
-      .post<Asset>(this.hostURL + subURl, asset)
+      .post<Asset>(url, asset)
+      .toPromise()
+      .then(data => console.log('data', data))
+      .catch(error => console.log('error', error));
+  };
+
+  getAllAssets = () => {
+    const url = `${environment.backendURL}asset/all`;
+    this.http
+      .get(url)
       .toPromise()
       .then(data => console.log('data', data))
       .catch(error => console.log('error', error));
