21c7044816c04fabd83beaec87a45098b417cb8d
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-05 09:58:40 +0530

    Query all assets

--- src/app/pool/pool.page.ts
+++ src/app/pool/pool.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AssetService } from '../services/Asset/asset.service';
 
 @Component({
   selector: 'app-pool',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoolPage implements OnInit {
   loaded = false;
-  constructor() {}
+  constructor(private asset: AssetService) {
+    this.asset.getAllAssets();
+  }
 
   ngOnInit() {}
 
