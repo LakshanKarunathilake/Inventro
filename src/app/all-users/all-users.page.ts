459ea134458f445d91e3b813ad997e1a16da156e
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-05 12:53:13 +0530

    Filter user array

--- src/app/all-users/all-users.page.ts
+++ src/app/all-users/all-users.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../services/Employee/employee.service';
 import { Observable } from 'rxjs';
 import { Employee } from 'src/models/Employee';
+import { filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-all-users',
@@ -10,9 +11,31 @@ import { Employee } from 'src/models/Employee';
 })
 export class AllUsersPage implements OnInit {
   employees: Observable<Employee[]>;
+  filteredEMployees: Observable<Employee[]>;
   constructor(private employee: EmployeeService) {}
 
   ngOnInit() {
     this.employees = this.employee.getAllEmployees();
+    this.filteredEMployees = this.employee.getAllEmployees();
   }
+
+  filterUsers = val => {
+    console.log('val', val);
+    if (val !== '') {
+      console.log('in');
+      this.employees = this.employees.pipe(
+        tap((emp: Employee[]) => {
+          console.log('emp', emp);
+          emp.filter(element => {
+            console.log('element', element);
+            return (
+              element.firstname.includes(val) || element.lastname.includes(val)
+            );
+          });
+        })
+      );
+    } else {
+      this.employees = this.filteredEMployees;
+    }
+  };
 }
