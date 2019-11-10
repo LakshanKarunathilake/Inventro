9f30bd6965f0cbefb42909253eb5fc4b38a28218
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-10 19:20:02 +0530

    Fix user filter issue

--- src/app/all-users/all-users.page.ts
+++ src/app/all-users/all-users.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../services/Employee/employee.service';
 import { Observable } from 'rxjs';
 import { Employee } from 'src/models/Employee';
-import { filter, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-all-users',
@@ -19,23 +19,24 @@ export class AllUsersPage implements OnInit {
     this.filteredEMployees = this.employee.getAllEmployees();
   }
 
-  filterUsers = val => {
+  filterUsers = (val: string) => {
     console.log('val', val);
     if (val !== '') {
       console.log('in');
-      this.employees = this.employees.pipe(
-        tap((emp: Employee[]) => {
+      this.filteredEMployees = this.employees.pipe(
+        map((emp: Employee[]) => {
           console.log('emp', emp);
-          emp.filter(element => {
+          return emp.filter(element => {
             console.log('element', element);
             return (
-              element.firstname.includes(val) || element.lastname.includes(val)
+              element.firstname.toLowerCase().includes(val.toLowerCase()) ||
+              element.lastname.toLowerCase().includes(val.toLowerCase())
             );
           });
         })
       );
     } else {
-      this.employees = this.filteredEMployees;
+      this.filteredEMployees = this.employees;
     }
   };
 }
