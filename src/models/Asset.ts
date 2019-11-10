839be4e72548d493bd8915004725af740cecb549
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-10 21:09:02 +0530

    Update asset model

--- src/models/Asset.ts
+++ src/models/Asset.ts
@@ -1,10 +1,23 @@
 export interface Asset {
   id?: string;
-  assetCategory: string;
+  assetId: string;
+  assetcategory: string;
   brandName: string;
   boughtCompanyName: string;
+  boughtCompanyAddress: string;
+  location: string;
+  companyContact: string;
   buyingPrice: string;
   warrantyStatus: string;
   boughtDate: string;
   description: string;
+  yrs: string;
+  months: string;
+  quantity: number;
+  displaySize?: string;
+  ram?: string;
+  capacity?: string;
+  categoryTypes?: string;
+  processor?: string;
+  broken?: string;
 }
