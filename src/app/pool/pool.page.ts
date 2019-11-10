6587de471fb960b3ea8f712d9d00585339b277d5
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-10 21:08:28 +0530

    Add filtering feature

--- src/app/pool/pool.page.ts
+++ src/app/pool/pool.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AssetService } from '../services/Asset/asset.service';
+import { Observable } from 'rxjs';
+import { Asset } from 'src/models/Asset';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pool',
@@ -7,14 +10,33 @@ import { AssetService } from '../services/Asset/asset.service';
   styleUrls: ['./pool.page.scss']
 })
 export class PoolPage implements OnInit {
-  loaded = false;
+  loading = false;
+  assets: Observable<Asset[]>;
+  filteredAssets: Observable<Asset[]>;
   constructor(private asset: AssetService) {
-    this.asset.getAllAssets();
+    this.assets = this.asset.getAllAssets();
+    this.filteredAssets = this.asset.getAllAssets();
   }
 
   ngOnInit() {}
 
-  selectAssetsRelatedToCategory = () => {
-    this.loaded = !this.loaded;
+  filterAssets = (val: string) => {
+    console.log('val', val);
+    if (val !== '') {
+      console.log('in');
+      this.filteredAssets = this.assets.pipe(
+        map((emp: Asset[]) => {
+          console.log('emp', emp);
+          return emp.filter(element => {
+            console.log('element', element);
+            return element.assetcategory
+              .toLowerCase()
+              .includes(val.toLowerCase());
+          });
+        })
+      );
+    } else {
+      this.filteredAssets = this.assets;
+    }
   };
 }
