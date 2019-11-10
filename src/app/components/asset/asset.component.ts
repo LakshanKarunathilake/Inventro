807e2d9cd99f2eb807fa98f588b6ab23d2834e1c
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-10 21:08:01 +0530

    Update views for assets

--- src/app/components/asset/asset.component.ts
+++ src/app/components/asset/asset.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Asset } from 'src/models/Asset';
 
 @Component({
   selector: 'asset',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asset.component.scss']
 })
 export class AssetComponent implements OnInit {
+  @Input() asset: Asset;
   constructor() {}
 
   ngOnInit() {}
