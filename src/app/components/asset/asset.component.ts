a2910a8c97a1feb619cc26f487df0d50bf029e2f
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-11 00:35:26 +0530

    Add booking and reserving

--- src/app/components/asset/asset.component.ts
+++ src/app/components/asset/asset.component.ts
@@ -1,5 +1,7 @@
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit, Input } from '@angular/core';
 import { Asset } from 'src/models/Asset';
+import { AssetService } from 'src/app/services/Asset/asset.service';
 
 @Component({
   selector: 'asset',
@@ -8,7 +10,57 @@ import { Asset } from 'src/models/Asset';
 })
 export class AssetComponent implements OnInit {
   @Input() asset: Asset;
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private assetService: AssetService
+  ) {}
 
   ngOnInit() {}
+
+  async bookAsset(type: string, asset: Asset) {
+    const from = new Date();
+    const numberOfDaysToAdd = 14;
+    from.setDate(from.getDate() + numberOfDaysToAdd);
+    console.log('from', from);
+
+    const alert = await this.alertController.create({
+      header: 'Mention the From Date, To Date and a message!',
+      inputs: [
+        {
+          name: 'From Date',
+          label: 'From Date',
+          type: 'date',
+          value: '2019-10-11'
+        },
+        {
+          label: 'To Date',
+          name: 'To Date',
+          type: 'date'
+        },
+        {
+          label: 'Description',
+          name: 'Description',
+          type: 'text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            this.assetService.placeBook();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
