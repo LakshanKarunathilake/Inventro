a653c022a7babaa9718d5a2dc1649a4fac2c1ffe
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-11 19:30:37 +0530

    Generate complain page

---
+++ src/app/complain/complain.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complain',
+  templateUrl: './complain.page.html',
+  styleUrls: ['./complain.page.scss'],
+})
+export class ComplainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
