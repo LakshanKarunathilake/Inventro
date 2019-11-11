f5770d638cc7746222eb83c1443326742a9b1db8
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-11 09:30:01 +0530

    Add book asset model

---
+++ src/models/BookAsset.ts
@@ -0,0 +1,12 @@
+export interface BookAsset {
+  massege: string;
+  username: string;
+  beginDate: string;
+  dueDate: string;
+  id?: string;
+  assetId: string;
+  description: string;
+  assetcategory: string;
+  requestedNic: string;
+  notificationType?: string;
+}
