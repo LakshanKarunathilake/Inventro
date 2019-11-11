4ff62976e627efedbbc30a6fae9af3f1a47d9979
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-12 00:44:48 +0530

    Update request page

--- src/app/requests/requests.page.ts
+++ src/app/requests/requests.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../services/Employee/employee.service';
+import { BookAsset } from 'src/models/BookAsset';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.page.html',
-  styleUrls: ['./requests.page.scss'],
+  styleUrls: ['./requests.page.scss']
 })
 export class RequestsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  bookings: Observable<BookAsset[]>;
+  requests: Observable<BookAsset[]>;
+  constructor(private employee: EmployeeService) {
+    this.bookings = this.employee.getBookings();
+    this.requests = this.employee.getRequests();
+    this.employee.getBookings().subscribe(data => console.log('data', data));
   }
 
+  ngOnInit() {}
 }
