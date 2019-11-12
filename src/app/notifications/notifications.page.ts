1a9096c9fb42becefc6f2a6552945b5e2d7503dd
Lakshan Karunathilake <manualsplash@gmail.com>
2019-11-12 13:36:05 +0530

    View for notifications

--- src/app/notifications/notifications.page.ts
+++ src/app/notifications/notifications.page.ts
@@ -1,4 +1,9 @@
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakDown } from 'src/models/BreakDown';
+import { BookAsset } from 'src/models/BookAsset';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-notifications',
@@ -6,14 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss']
 })
 export class NotificationsPage implements OnInit {
-  dummyNotifications = [
-    { title: 'Accepted', message: 'sdsadsahdsajhd' },
-    { title: 'Accepted', message: 'sdsadsahdsajhd' },
-    { title: 'Accepted', message: 'sdsadsahdsajhd' },
-    { title: 'Accepted', message: 'sdsadsahdsajhd' },
-    { title: 'Accepted', message: 'sdsadsahdsajhd' }
-  ];
-  constructor() {}
+  breakDownRecords: Observable<BreakDown[]>;
+  bookings: Observable<BookAsset[]>;
+  constructor(private afs: AngularFirestore) {
+    this.breakDownRecords = this.afs
+      .collection<BreakDown>('breakDown')
+      .valueChanges();
+    this.bookings = this.afs
+      .collection<BookAsset>('BookAssetNotification')
+      .valueChanges()
+      .pipe(
+        map(val => {
+          console.log('val', val);
+          return val;
+        })
+      );
+  }
 
   ngOnInit() {}
 }
